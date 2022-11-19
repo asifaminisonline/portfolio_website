0936b527ee1c899e6e4c695573468e0b4b4a2d25
asifaminisonline <asifaminisonline@gmail.com>
2022-11-19 19:23:21 +0530

    Added Popup functionality in portfolio

--- localStorage.js
+++ localStorage.js
@@ -1,24 +1,24 @@
-var makeInput = document.getElementById("make");
-var emailInput = document.getElementById("email");
+const makeInput = document.getElementById('make');
+const emailInput = document.getElementById('email');
 
-var storageInfo = JSON.parse(localStorage.getItem("storageInfo"));
+let storageInfo = JSON.parse(localStorage.getItem('storageInfo'));
 if (storageInfo) {
   makeInput.value = storageInfo.make;
   emailInput.value = storageInfo.email;
 }
 document
-  .getElementById("register-form")
-  .addEventListener("submit", function () {
-    var make = makeInput.value.trim();
-    var email = emailInput.value.trim();
+  .getElementById('register-form')
+  .addEventListener('submit', () => {
+    const make = makeInput.value.trim();
+    const email = emailInput.value.trim();
 
     if (!make || !email) {
       return;
     }
 
-    var storageInfo = {
-      email: email,
-      make: make,
+    storageInfo = {
+      email,
+      make,
     };
-    localStorage.setItem("storageInfo", JSON.stringify(storageInfo));
+    localStorage.setItem('storageInfo', JSON.stringify(storageInfo));
   });
